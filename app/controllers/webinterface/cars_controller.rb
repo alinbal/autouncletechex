04a5ca64f7887cfffc844d614aec296ca434004d
Alin Baltatescu <alinbaltatescu@Alins-Mac-mini.local>
2020-09-23 01:57:59 +0200

    added search functionality for webinterface

--- app/controllers/webinterface/cars_controller.rb
+++ app/controllers/webinterface/cars_controller.rb
@@ -2,9 +2,12 @@ module Webinterface
   class CarsController < ApplicationController
     def index
       @cars = Car.all # creates an anonymous scope
-      @cars = @cars.filter_by_brand(params[:brand]) if params[:brand].present?
-      @cars = @cars.filter_by_model(params[:model]) if params[:model].present?
-      @cars = @cars.filter_by_year(params[:year]) if params[:year].present?
+      @search = params["search"]
+      if @search.present?
+        @cars = @cars.filter_by_brand(@search['brand']) if @search['brand'].present?
+        @cars = @cars.filter_by_model(@search['model']) if @search['model'].present?
+        @cars = @cars.filter_by_year(@search['year']) if @search['year'].present?
+      end
     end
   end
 end
