d90b7fc09952b14ffa7262b9766f8af1e3878b74
Alin Baltatescu <alinbaltatescu@Alins-Mac-mini.local>
2020-09-22 23:02:22 +0200

    added filtering based on model, year, brand

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -4,8 +4,11 @@ module Api
   module V1
     class CarsController < ApplicationController
       def index
-        cars = Car.order('created_at DESC')
-        render json: {status: 'SUCCESS', message: 'Loaded Cars', data: cars}, status: :ok
+        @cars = Car.where(nil) # creates an anonymous scope
+        @cars = @cars.filter_by_brand(params[:brand]) if params[:brand].present?
+        @cars = @cars.filter_by_model(params[:model]) if params[:model].present?
+        @cars = @cars.filter_by_year(params[:year]) if params[:year].present?
+        render json: {status: 'SUCCESS', message: 'Loaded Cars', data: @cars}, status: :ok
       end
     end
   end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Car < ApplicationRecord
+  scope :filter_by_brand, ->(brand) { where brand: brand }
+  scope :filter_by_model, ->(model) { where car_model_name: model }
+  scope :filter_by_year, ->(year) { where year: year }
   has_many :adverts
   has_many :image_urls
 end
